ff79605c348c74e7098fb7e245856cc1980cf3b4
bmsk <vv99911@gmail.com>
2023-06-14 10:10:45 +0900

    Update Request2Background.kt

--- src/tasks/Request2Background.kt
+++ src/tasks/Request2Background.kt
@@ -5,8 +5,12 @@ import contributors.RequestData
 import contributors.User
 import kotlin.concurrent.thread
 
+/**
+ * 이 방식은 하나의 콜백 함수가 있다면 다음 콜백은 이전 콜백을 기다려야 한다.
+ */
 fun loadContributorsBackground(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     thread {
-        loadContributorsBlocking(service, req)
+        val users = loadContributorsBlocking(service, req)
+        updateResults(users)
     }
 }
